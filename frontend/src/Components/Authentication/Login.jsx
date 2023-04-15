12dbbedc1c9235adc4d3e382616e64472d710870
Chinmay Pathak <pathakchinmay02@gmail.com>
2023-04-15 23:53:45 +0530

    .

--- frontend/src/Components/Authentication/Login.jsx
+++ frontend/src/Components/Authentication/Login.jsx
@@ -1,5 +1,64 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Show,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState} from "react";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+    
+  const submitHandler = () => {};
+  return (
+    <VStack>
+      <FormControl>
+        <FormLabel id="email" isrequired="true">
+          Email
+        </FormLabel>
+        <Input
+          type="email"
+          placeholder="Enter your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+
+      <FormControl id="password" isrequired="true">
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5">
+            <Button h="1.75" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width={"100%"}
+        style={{
+          marginTop: 15,
+        }}
+        onClick={submitHandler}
+      >
+        Login
+      </Button>
+    </VStack>
+  );
 };
